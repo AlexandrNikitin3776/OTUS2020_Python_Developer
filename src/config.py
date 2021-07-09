2d1dffcb43f234bcfbcaf9eb5fdda3262ea7d614
anikitin <anikitin@ubic.tech>
2021-07-10 00:55:16 +0300

    Parse arguments from command prompt

--- src/config.py
+++
@@ -1,9 +0,0 @@
-class LogConfig:
-    REPORT_SIZE: int = 1000
-    REPORT_DIR: str = "./reports"
-    LOG_DIR: str = "./log"
-
-    def update(self, config_args: dict):
-        # Проверить, что config_args не пустой
-        # Проверить, что в config_args содержатся только аргументы из списка переменных
-        self.__dict__.update(config_args)
